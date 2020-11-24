69e253be2b51493095ee797387d98cc18a0fb224
binwei0329 <williambinwei@hotmail.com>
2020-11-24 16:43:52 +0800

    remove-relevant methods updated

--- Graph/MyGraph.java
+++ Graph/MyGraph.java
@@ -1,13 +1,22 @@
 import java.util.Iterator;
 
+/**
+ *本类实现了图这个数据结构以及其主要方法。
+ *
+ * @param <T> 类型参数
+ * @author Wei Bin
+ * @date 2020/11/24
+ */
+
+@SuppressWarnings("all")
 public class MyGraph<T> implements MyGraphInterface<T> {
     protected final int DEFAULT_CAPACITY = 8;
-    protected int numVertices; //顶点数量
+    protected int num; //顶点数量
     protected boolean adjMat[][]; //邻接矩阵
     protected T[] vertices; //顶点
 
     public MyGraph(){
-        numVertices = 0;
+        num = 0;
         adjMat = new boolean[DEFAULT_CAPACITY][DEFAULT_CAPACITY];
         vertices = (T[]) (new Object[DEFAULT_CAPACITY]);
     }
@@ -19,19 +28,19 @@ public class MyGraph<T> implements MyGraphInterface<T> {
 
     @Override
     public void addVertex(T ele) {
-        if(numVertices == 0){
+        if(num == 0){
             vertices[0] = ele;
-            numVertices++;
+            num++;
         }
         else{
-            vertices[numVertices] = ele;
-            numVertices++;
+            vertices[num] = ele;
+            num++;
         }
     }
 
     @Override
     public void removeVertex(T ele) {
-        if(numVertices == 0){
+        if(num == 0){
             throw new RuntimeException("Empty graph exception.");
         }
         else{
@@ -41,12 +50,49 @@ public class MyGraph<T> implements MyGraphInterface<T> {
             }
             else {
                 vertices[index] = null;
-
-                for(int i = index; i < numVertices - 1; i++){
+                for(int i = index; i < num - 1; i++){
                     vertices[i] = vertices[i + 1];
+                }
+                vertices[num - 1] = null;
+                adjMat = removeVertexEdge(index);
+                num--;
+            }
+        }
+    }
+
+    private boolean[][] removeVertexEdge(int index){
+        boolean[][] temp = new boolean[num - 1][num - 1];
+        if(index < 0){
+            throw new RuntimeException("Vertex not found exception.");
+        }
+        else {
+            for(int m = 0; m < num; m++){
+                if(m < index) {
+                    boolean[] array = new boolean[num - 1];
+                    boolean[] vec = adjMat[m];
+                    for (int k = 0; k < index; k++) {
+                        array[k] = vec[k];
+                    }
+                    for (int k = index; k < num - 1; k++) {
+                        array[k] = vec[k + 1];
+                    }
+                    temp[m] = array;
+                }
+
+                else if(m > index){
+                    boolean[] array = new boolean[num - 1];
+                    boolean[] vec = adjMat[m];
+                    for (int k = 0; k < index; k++) {
+                        array[k] = vec[k];
+                    }
+                    for (int k = index; k < num - 1; k++) {
+                        array[k] = vec[k + 1];
+                    }
+                    temp[m - 1] = array;
                 }
             }
         }
+        return temp;
     }
 
     @Override
@@ -77,7 +123,7 @@ public class MyGraph<T> implements MyGraphInterface<T> {
 
     private int getIndex(T ele){
         int index = -1;
-        for(int ind = 0; ind < numVertices; ind++){
+        for(int ind = 0; ind < num; ind++){
             if(vertices[ind] == ele){
                 index = ind;
                 break;
@@ -108,11 +154,19 @@ public class MyGraph<T> implements MyGraphInterface<T> {
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return num == 0;
     }
 
     @Override
     public int getSize() {
-        return numVertices;
+        return num;
+    }
+
+    public String toString(){
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < num; i++) {
+            builder.append(vertices[i] + " ");
+        }
+        return builder.toString();
     }
 }
